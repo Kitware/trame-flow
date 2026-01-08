3b73e5cb267ea8ec0002898b2d8cc3f5a072b679
Corentin MUNOZ <corentin.munoz@kitware.com>
2026-01-08 18:32:35 +0100

    ci: fix ci

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,5 +1,3 @@
 def test_import():
-    # For components only, the CustomWidget is also importable via trame
-    from trame.widgets.trame_flow import NodeEditor
-
+    from trame.widgets.flow import NodeEditor
     from trame_flow.widgets.flow import NodeEditor  # noqa: F401,F811
